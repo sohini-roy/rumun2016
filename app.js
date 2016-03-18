061195f726ab42b83203f208a64f6eb1b90f32d0
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2016-03-18 21:00:19 +0530

    Added template

--- app.js
+++ app.js
@@ -5,14 +5,12 @@
 
 var express = require('express')
   , routes = require('./routes');
-
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
---
+++ views/php/config.php
@@ -0,0 +1,15 @@
+<?php
+	
+$me = array(
+    'email' => 'yegorborisenco@gmail.com',
+    'name' => 'Yegor Borisenco'
+);
+
+$smtp = array(
+    'enabled' => false,
+    'host' => 'smtp.gmail.com',
+    'username' => 'yegorborisenco@gmail.com',
+    'password' => 'net1818work',
+    'port' => 587,
+    'tls' => true
+);
